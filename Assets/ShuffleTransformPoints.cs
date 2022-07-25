3a726a586e3726a9b0a95021b8e61f3fd2e95001
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-25 13:19:09 +0300

    ss

--- Assets/ShuffleTransformPoints.cs
+++ Assets/ShuffleTransformPoints.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 
 public class ShuffleTransformPoints : MonoBehaviour
 {
-    public GameObject spawnPoint;
-     GameObject shuffle;
-    private float minDistance= 6f;
-    private float MaxDistance = 10f;
-    private float minX = -6f, maxX = 6f;
-    public Vector3 pos;
-
-    public Transform[] spawnpoints;
+      Transform spawnPoint;
+      public Transform[] spawnpoints;
+     public GameObject point;
+     public Vector3 pos;
+  
   
     private void Start()
     {
-        StartCoroutine(ShufflePoints());
-    }
-    IEnumerator ShufflePoints()
-    {
+        int index = Random.Range(0, spawnpoints.Length);
+        spawnPoint = spawnpoints[index];
+        for (int i = 0; i < spawnpoints.Length; i++)
+        {
 
-        //3 points only
-   
-            pos = transform.position;
-            pos.x = Random.Range(minX, maxX);
-            pos.z += Random.Range(minDistance, MaxDistance);
-            shuffle = Instantiate(spawnPoint, pos, Quaternion.identity); //this seems to work now, WTF??
-                                                              //assigning the position for every new objec
+            if (i == index)
+            {
+                spawnpoints[index].gameObject.SetActive(true);
 
-        yield return null;
+            }
+            else
+            {
+                spawnpoints[i].gameObject.SetActive(false);
+            }
+        }
+        // Instantiate(spawnPoint, spawnPoint.transform.position, Quaternion.identity); //this seems to work now, WTF??
     }
+
+    //assigning the position for every new objec
 
 
 }
