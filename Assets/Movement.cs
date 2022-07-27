ee762b8eef00e3c1238ac1175b3eb19a7d06eda2
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-27 13:12:39 +0300

    ss

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -9,8 +9,6 @@ public class Movement : MonoBehaviour
     public float speed = 20f;
     public float JumpSpeed = 5f;
     bool isOntheGround = false;
-   public bool Mode1Automatic;
-   public bool Mode2Manual;
     Rigidbody rb;
     void Start()
     {
@@ -21,35 +19,19 @@ public class Movement : MonoBehaviour
     void Update()
     {
 
-        CheckforMode();
         float Horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             float Vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         /* transform.Translate(Horizontal, 0, Vertical); */ //initializing it with new Vector3 or you can simply use Horizontal, 0, Vertical too
         transform.Translate(Horizontal, 0, Vertical);     
-        if(Input.GetButtonDown("Jump") && isOntheGround)
-                {
-
-                     rb.AddForce(transform.up * speed, ForceMode.Impulse);
-                    isOntheGround = false;
-                }
+       if(Input.GetButtonDown("Jump") && isOntheGround)
+        {
+            rb.AddForce(transform.up * JumpSpeed, ForceMode.Impulse);
+            isOntheGround = false;
+        }
 
        
 
 
-    }
-
-    void CheckforMode()
-    {
-        if (Input.GetKeyDown(KeyCode.M) && Mode1Automatic)
-        {
-            Mode1Automatic = false;
-            Mode2Manual = true;
-        }
-        if (Input.GetKeyDown(KeyCode.M) && Mode2Manual)
-        {
-            Mode1Automatic = true;
-            Mode2Manual = false;
-        }
     }
 
     private void OnCollisionEnter(Collision collision)  //for JUMPS IMMA USE COLLISION DETECTOR NOW. THIS IS SO MUCH FUCKING BETTER!!!
