54add91d91a7d6dd7e8e74be2aca7dcbd57ebbce
Muhammad Bilal <m.bilal9862@gmail.com>
2022-07-29 12:52:13 +0300

    ss

--- Assets/MovingOsbtacle.cs
+++ Assets/MovingOsbtacle.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class MovingOsbtacle : MonoBehaviour
 {
-    public Transform Rightbound;
-    public Transform LeftBound;
+     Transform Rightbound;
+     Transform LeftBound;
     public GameObject ground;
-
+     Vector3 position;
     // Update is called once per frame
 
     private void Awake()
@@ -15,10 +15,26 @@ public class MovingOsbtacle : MonoBehaviour
         ground = GameObject.FindWithTag("Ground");
         Rightbound = ground.transform.GetChild(8);
         LeftBound = ground.transform.GetChild(9); //this is how you get childs
+        position = LeftBound.position;
 
     }
     void Update()
     {
-        
+        transform.Translate(position * Time.deltaTime);
+
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name=="BoundLeft")
+        {
+
+        }
+        if (collision.collider.name == "BoundLeft")
+        {
+
+        }
     }
 }
