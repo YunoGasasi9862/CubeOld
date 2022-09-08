95302a613c33b6fb9d2f85f6f4b5b42c09b25bf5
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-09-08 14:18:12 +0300

    ss

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -5,7 +5,7 @@ public class CameraRotation : MonoBehaviour
 {
     public float MouseSensitivity = 200f;
     public GameObject Cube;
-    float yRotation;
+    float XRotation;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -18,13 +18,14 @@ public class CameraRotation : MonoBehaviour
         float MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
         float MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
 
-        yRotation -= MouseY;
 
-        yRotation = Mathf.Clamp(yRotation, -45, 45);
+        XRotation -= MouseY;  //x rotation (against y axis)
 
-        transform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+        XRotation = Mathf.Clamp(XRotation, -45, 45);
 
-        Cube.transform.Rotate(Vector3.up * MouseX);
+        transform.localRotation = Quaternion.Euler(XRotation, 0, 0);
+
+        Cube.transform.Rotate(Vector3.up * MouseX); //y axis against X axix
 
 
         //the is similar to transform.Rotate (0,0,4);
