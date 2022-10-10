d79078d01390ada14423f99cc2b6d18794d8a283
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-10-10 12:57:25 +0300

    ss

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -6,8 +6,9 @@ public class FollowPlayer : MonoBehaviour
 {
     private GameObject Player;
     private Rigidbody rb;
-    float speed = 50f;
-
+    [SerializeField]float speed = 50f;
+    private Vector3 previousPosition;
+        
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,11 +19,25 @@ public class FollowPlayer : MonoBehaviour
 
 
         //check tomorrow how to move it without using Vector3.movetowards
-          transform.position= Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+     
+          if(Vector2.Distance(transform.position, Player.transform.position)>=30)
+         {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, speed * Time.deltaTime);
+            previousPosition = transform.position;
+
+        }
+        else
+          {
+            transform.position = Vector3.MoveTowards(previousPosition, Player.transform.position, speed * Time.deltaTime);
+
+
+        }
+
 
         //using the DotProduct method, and rotating it on that Axis only
 
         float DotProduct = Vector3.Dot(transform.position, Player.transform.position);
+
 
         DotProduct = DotProduct / (transform.position.magnitude * Player.transform.position.magnitude);
 
@@ -32,26 +47,17 @@ public class FollowPlayer : MonoBehaviour
 
 
         //if need in degrees
-        float angleinDegrees = (angleinRadians) * (180 / Mathf.PI);
+        float angleinDegrees = angleinRadians * (180 / Mathf.PI);
 
-        transform.rotation = Quaternion.AngleAxis(angleinDegrees, Vector3.right);
-         
+        float PreviousRotationAngle = angleinDegrees;
+
+        transform.rotation = Quaternion.AngleAxis(PreviousRotationAngle, Vector3.right);
+
         
+         
 
-
-
-      /**
-            //for rotating in that direction
-
-        Vector3 RotateDirection = Player.transform.position - transform.position;
-
-        //fromToRotation is must (because it assigns the rotation => Rotate the X axis toward the target 
-        Quaternion rotation = Quaternion.FromToRotation(transform.right, RotateDirection);
-
-
-        //Lerp simply interpolates the rotation => current transform.rotation to rotation which we calculated before in this amount of time
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime);
-
-        **/
+       
     }
+
+
 }
