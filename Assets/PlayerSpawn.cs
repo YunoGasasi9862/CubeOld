fa6bc5a7687e35f3c3fef65f6b9e4636bf14f0a6
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2022-12-01 13:49:10 +0300

    ss

--- Assets/PlayerSpawn.cs
+++ Assets/PlayerSpawn.cs
@@ -6,18 +6,50 @@ public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] Camera cam;
     [SerializeField] GameObject Astronaut;
-
+    private Vector3 spawnPoint;
+    [SerializeField] LayerMask Player;
+    private RaycastHit hit;
     void Start()
     {
         cam = GameObject.FindWithTag("TemporaryCamera").GetComponent<Camera>();
         Invoke("InstantiatePlayer", 2f);
     }
 
+ 
     public void InstantiatePlayer()
     {
         //bring Player from above!!
-        Instantiate(Astronaut, transform.position, Quaternion.identity);
-        cam.enabled = false;
+        spawnPoint = transform.position;
+        spawnPoint.y = transform.position.y + 10f; //the height from which the player drops
+        Instantiate(Astronaut, spawnPoint, Quaternion.identity);
+
+    }
+    private void Update()
+    {
+        RayCast();
+
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if(collision.collider.CompareTag("Player"))
+    //    {
+    //        collision.gameObject.transform
+    //        Debug.Log("Meow");
+    //    }
+    //}
+    public void RayCast()
+    {
+      
+        Physics.Raycast(transform.position, transform.up, out hit, 3f, Player);
+
+        Debug.DrawRay(transform.position, transform.up * 3f, Color.red);
+
+        if(hit.collider!=null)
+        {
+            hit.collider.gameObject.transform.GetChild(2).transform.GetChild(0).GetComponent<Camera>().enabled = true;
+
+        }
 
     }
 
